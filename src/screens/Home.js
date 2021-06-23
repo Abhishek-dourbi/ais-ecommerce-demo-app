830de7e824bfa65d5546a7677f803c0441e54fcf
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-06-23 21:04:26 +0530

    fix code formatting

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -125,118 +125,118 @@ class Home extends React.Component {
     }
 
     render() {
-    const { isModalOpen, searchState, selectedGender } = this.state;
+        const { isModalOpen, searchState, selectedGender } = this.state;
 
-    return (
-        <SafeAreaView style={styles.safe}>
-            <StatusBar barStyle="light-content" />
-            <View style={styles.container}>
-            <View style={{
-                flexDirection: 'row',
-                alignSelf: 'flex-start',
-                marginBottom: 20
-            }}>
-            {
-                genders.map(({label, value}) => {
-                    return (
-                        <TouchableOpacity onPress={() => this.onGenderPress(value)} style={{
-                            backgroundColor: selectedGender === value ? 'grey' : 'white',
-                            borderColor: 'black',
-                            borderWidth: 2,
-                            marginRight: 10,
-                            borderRadius: 10,
-                            padding: 10
-                        }}>
-                        <Text style={{
-                            color: selectedGender === value ? 'white' : 'black'
-                        }}>
-                            {label}
-                        </Text>
-                        </TouchableOpacity>
-                    )
-                })
-            }
-            </View>
-            <TextInput 
-                onChangeText={this.onChangeText} 
-                style={{
-                    width: '100%', 
-                    height: 50, 
-                    color: '#000',
-                    fontSize: 18,
-                    borderColor: '#000',
-                    borderWidth: 2,
-                    borderTopLeftRadius: 10,
-                    borderTopRightRadius: 10,
-                    padding: 10
-                }}
-                placeholder='Search..'
-            />
-            <View style={{width: '100%', backgroundColor: 'grey', borderBottomLeftRadius: 10, borderBottomRightRadius: 10,}}>
-                {
-                    this.state.topSearches.length > 0 &&
-                        <View style={{padding: 10}}>
-                            <Text style={{fontSize: 20, color: '#fff', fontWeight: 'bold', marginBottom: 10, }}>
-                                Top Searches
-                            </Text>
-                            <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-                            {
-                                this.state.topSearches.map(ele => {
-                                    return (
-                                    <Text style={{color: 'yellow', textDecorationLine: 'underline', marginBottom: 10, marginRight: 10}}>
-                                        {ele.search}
-                                    </Text>
-                                    )
-                                })
-                            }
-                            </View>
-                        </View>
-                }
-                {
-                    this.state.recentSearches.length > 0 &&
-                        <View style={{padding: 10}}>
-                            <Text style={{fontSize: 20, color: '#fff', fontWeight: 'bold', marginBottom: 10, }}>
-                                Recent Searches
-                            </Text>
-                            <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-                            {
-                            this.state.recentSearches.map(ele => {
-                                return (
-                                <Text style={{color: 'yellow', textDecorationLine: 'underline', marginBottom: 10, marginRight: 10}}>
-                                    {ele}
+        return (
+            <SafeAreaView style={styles.safe}>
+                <StatusBar barStyle="light-content" />
+                <View style={styles.container}>
+                    <View style={{
+                        flexDirection: 'row',
+                        alignSelf: 'flex-start',
+                        marginBottom: 20
+                    }}>
+                    {
+                        genders.map(({label, value}) => {
+                            return (
+                                <TouchableOpacity onPress={() => this.onGenderPress(value)} style={{
+                                    backgroundColor: selectedGender === value ? 'grey' : 'white',
+                                    borderColor: 'black',
+                                    borderWidth: 2,
+                                    marginRight: 10,
+                                    borderRadius: 10,
+                                    padding: 10
+                                }}>
+                                <Text style={{
+                                    color: selectedGender === value ? 'white' : 'black'
+                                }}>
+                                    {label}
                                 </Text>
-                                )
-                            })
-                            }
-                            </View>
-                        </View>
-                }
-                {
-                this.state.hits.map(ele => {
-                    return (
-                        <TouchableOpacity 
-                            style={{
-                            padding: 10, 
-                            borderBottomColor: '#fff', 
-                            borderBottomWidth: 2,
-                            flexDirection: 'row',
-                            justifyContent: 'space-between'
-                        }} 
-                        onPress={() => this.onSuggestionPress(ele)}>
-                            <Text style={{fontSize: 18}}>
-                                {ele.query}
-                            </Text>
-                            <Text>
-                                {ele[sourceIndexName].exact_nb_hits}
-                            </Text>
-                        </TouchableOpacity>
-                    )
-                })
-                }
-            </View>
-            </View>
-        </SafeAreaView>
-    );
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+                    </View>
+                    <TextInput 
+                        onChangeText={this.onChangeText} 
+                        style={{
+                            width: '100%', 
+                            height: 50, 
+                            color: '#000',
+                            fontSize: 18,
+                            borderColor: '#000',
+                            borderWidth: 2,
+                            borderTopLeftRadius: 10,
+                            borderTopRightRadius: 10,
+                            padding: 10
+                        }}
+                        placeholder='Search..'
+                    />
+                    <View style={{width: '100%', backgroundColor: 'grey', borderBottomLeftRadius: 10, borderBottomRightRadius: 10,}}>
+                        {
+                            this.state.topSearches.length > 0 &&
+                                <View style={{padding: 10}}>
+                                    <Text style={{fontSize: 20, color: '#fff', fontWeight: 'bold', marginBottom: 10, }}>
+                                        Top Searches
+                                    </Text>
+                                    <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                                    {
+                                        this.state.topSearches.map(ele => {
+                                            return (
+                                            <Text style={{color: 'yellow', textDecorationLine: 'underline', marginBottom: 10, marginRight: 10}}>
+                                                {ele.search}
+                                            </Text>
+                                            )
+                                        })
+                                    }
+                                    </View>
+                                </View>
+                        }
+                        {
+                            this.state.recentSearches.length > 0 &&
+                                <View style={{padding: 10}}>
+                                    <Text style={{fontSize: 20, color: '#fff', fontWeight: 'bold', marginBottom: 10, }}>
+                                        Recent Searches
+                                    </Text>
+                                    <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                                    {
+                                    this.state.recentSearches.map(ele => {
+                                        return (
+                                        <Text style={{color: 'yellow', textDecorationLine: 'underline', marginBottom: 10, marginRight: 10}}>
+                                            {ele}
+                                        </Text>
+                                        )
+                                    })
+                                    }
+                                    </View>
+                                </View>
+                        }
+                        {
+                        this.state.hits.map(ele => {
+                            return (
+                                <TouchableOpacity 
+                                    style={{
+                                    padding: 10, 
+                                    borderBottomColor: '#fff', 
+                                    borderBottomWidth: 2,
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between'
+                                }} 
+                                onPress={() => this.onSuggestionPress(ele)}>
+                                    <Text style={{fontSize: 18}}>
+                                        {ele.query}
+                                    </Text>
+                                    <Text>
+                                        {ele[sourceIndexName].exact_nb_hits}
+                                    </Text>
+                                </TouchableOpacity>
+                            )
+                        })
+                        }
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
     }
 }
 
